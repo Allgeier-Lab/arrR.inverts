3af7503ad8703e0653f4a8961065d5792875282e
Katrina Munsterman <kmunster@umich.edu>
2023-03-06 14:10:28 -0800

    First commit with initial files

---
+++ NEWS.md
@@ -0,0 +1,2 @@
+# arrR.inverts 0.1.0
+* Separate all invert environment functions
---
+++ R/arrR.inverts-packages.R
@@ -0,0 +1,13 @@
+#' @description
+#' Individual-based simulation model to analyze how artificial reefs in combination
+#' with a fish population influence seagrass growth.
+#'
+#' For more information about how to use the model, please see
+#' \url{https://allgeier-lab.github.io/arrR/}.
+#'
+#' @name arrR.inverts
+#' @docType package
+#' @useDynLib arrR.inverts, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
+#' @keywords internal
+"_PACKAGE"
